ac10ca287092e7dda998ddfb803ee659b355b16f
Taylor Gagne <taylor@baremetalcloud.com>
2017-05-09 11:51:09 -0400

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,2 @@
+// Includes a single default profile, so that anyone demoing from the GitHub repository will have a result.
+module.exports = [{"name":"Taylor","photo":"https://media4.giphy.com/media/w0CJXS2M44xfW/200_s.gif","answers":["5","2","2","3","2","5","2","5","3","1"]}];
---
+++ server.js
@@ -0,0 +1,25 @@
+// Import dependencies.
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Initialize app.
+var app = express();
+
+// PORT is either the port provided by Heroku via process.env.PORT or 3000.
+// var PORT = process.env.PORT || 3000;
+
+// Set up middleware.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static(__dirname + '/app/public'));
+
+// Import routes.
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+// Start listening.
+app.listen(3000, function() {
+    console.log('Application is running on port 3000')
+});
